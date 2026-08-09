e6d1b6d1ef7897442baeeef273e8dfe8dbc77420
Adam Hancock <adamjshancock@outlook.com>
2026-08-09 13:13:26 +0100

    Update trajectory.py

--- visualisations/trajectory.py
+++ visualisations/trajectory.py
@@ -1,17 +1,20 @@
-#from pendulums.double_pendulum import double_pendulum
 import matplotlib.pyplot as plt
 import numpy as np
 
 def trajectory(pendulum):
+    #plots the trace of the end mass
+
     l1 = pendulum.l1
     l2 = pendulum.l2
     theta1 = pendulum.theta1
     theta2 = pendulum.theta2
 
-    x = l1 * np.sin(theta1) + l2 *np.sin(theta2)
+    x1 = l1 * np.sin(theta1)
+    y1 = -l1 * np.cos(theta1)
+    x2 = x1 + l2 * np.sin(theta2)
+    y2 = y1 - l2 * np.cos(theta2)
 
-    y = l2 * np.cos(theta1) + l2 * np.cos(theta2)
+    plt.plot(x2,y2)
 
-    plt.plot(x,y)
+    plt.title("Trajectory: the trace of the end mass")
     plt.show()
-
